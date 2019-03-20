17656dedfa054db6acee01eb0ab7f1ee798b550a
hifizz <276161014@qq.com>
2019-03-20 15:45:17 +0800

    wip: avl-tree

---
+++ src/tree/avl-tree/index.ts
@@ -0,0 +1,13 @@
+import { BinarySearchTree } from '../binary-search-tree'
+import { BinarySearchNode } from '../binary-search-tree/BinarySearchNode'
+
+export class AVLTree extends BinarySearchTree {
+  public insert(): void {}
+  // public remove(value: any) {}
+  public balance() {}
+  /**  */
+  public rotateLeftLeft() {}
+  public rotateLeftRight() {}
+  public rotateRightLeft() {}
+  public rotateRightRight() {}
+}
